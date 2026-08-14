c049fec5d6ea13582490c669f3f15a82bd40675e
Md. Tushar Hossain <85397962+tushar-hossain@users.noreply.github.com>
2026-08-15 00:07:20 +0600

    Merchant CTA and  Customer Testimonials section design

--- src/Pages/LandingPage/index.jsx
+++ src/Pages/LandingPage/index.jsx
@@ -1,6 +1,8 @@
 import Banner from "./Components/Banner_Slider";
 import BrandsClient from "./Components/Brands_Client";
+import CustomerTestimonials from "./Components/Customertestimonials";
 import HowItWorks from "./Components/HowItWorks";
+import MerchantCTA from "./Components/MerchantCTA";
 import OurServices from "./Components/OurServices";
 import WhyChooseUs from "./Components/WhyChooseUs";
 
@@ -21,6 +23,12 @@ const Home = () => {
       </div>
       <div className="my-10">
         <WhyChooseUs />
+      </div>
+      <div className="my-10">
+        <MerchantCTA />
+      </div>
+      <div className="my-10">
+        <CustomerTestimonials />
       </div>
     </div>
   );
