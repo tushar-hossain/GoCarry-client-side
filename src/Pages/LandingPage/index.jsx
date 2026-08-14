7115764eec1300780c38c9673f5394c72cdc08c2
Md. Tushar Hossain <85397962+tushar-hossain@users.noreply.github.com>
2026-08-15 00:12:22 +0600

    Frequently Asked Question section design done

--- src/Pages/LandingPage/index.jsx
+++ src/Pages/LandingPage/index.jsx
@@ -1,6 +1,7 @@
 import Banner from "./Components/Banner_Slider";
 import BrandsClient from "./Components/Brands_Client";
 import CustomerTestimonials from "./Components/Customertestimonials";
+import Faq from "./Components/Faq";
 import HowItWorks from "./Components/HowItWorks";
 import MerchantCTA from "./Components/MerchantCTA";
 import OurServices from "./Components/OurServices";
@@ -29,6 +30,9 @@ const Home = () => {
       </div>
       <div className="my-10">
         <CustomerTestimonials />
+      </div>
+      <div className="my-10">
+        <Faq />
       </div>
     </div>
   );
