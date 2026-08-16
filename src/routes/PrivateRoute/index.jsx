bc93340edee8808bf95e723053c26072bacbbb66
Md. Tushar Hossain <85397962+tushar-hossain@users.noreply.github.com>
2026-08-16 21:02:12 +0600

    register and login firebase account create done

---
+++ src/routes/PrivateRoute/index.jsx
@@ -0,0 +1,23 @@
+import useAuth from "@/hooks/useAuth";
+import { Navigate } from "react-router";
+
+export default function PrivateRoutes({ children }) {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="flex items-center gap-2">
+          <div className="h-5 w-5 animate-spin rounded-full border-2 border-[#CAEB66] border-t-[#03373D]" />
+          <span className="text-sm text-[#03373D]">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to={"login"} />;
+  }
+
+  return children;
+}
