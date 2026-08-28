c384130fe1e4af6eb9918a7bb211ef6fc71a65c0
Md. Tushar Hossain <85397962+tushar-hossain@users.noreply.github.com>
2026-08-28 19:19:29 +0600

    Update useAxiosSecure.jsx

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,25 @@
 import axios from "axios";
+import useAuth from "./useAuth";
 
 const axiosSecureInstance = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 export default function useAxiosSecure() {
+  const { user } = useAuth();
+
+  axiosSecureInstance.interceptors.request.use(
+    (config) => {
+      const token = user?.accessToken;
+
+      if (token) {
+        config.headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      return config;
+    },
+    (error) => Promise.reject(error),
+  );
+
   return axiosSecureInstance;
 }
